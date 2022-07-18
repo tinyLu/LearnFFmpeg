0b8edec0c5709f6d51c0b35c409f332971c22b80
luxiangjun <luxiangjun@inspur.com>
2022-07-18 10:45:39 +0800

    修改调试地址，以及日志级别输出

--- app/src/main/java/com/byteflow/learnffmpeg/NativeMediaPlayerActivity.java
+++ app/src/main/java/com/byteflow/learnffmpeg/NativeMediaPlayerActivity.java
@@ -115,7 +115,7 @@ public class NativeMediaPlayerActivity extends AppCompatActivity implements Surf
         Log.d(TAG, "surfaceCreated() called with: surfaceHolder = [" + surfaceHolder + "]");
         mMediaPlayer = new FFMediaPlayer();
         mMediaPlayer.addEventCallback(this);
-        mMediaPlayer.init(mVideoPath, VIDEO_RENDER_ANWINDOW, surfaceHolder.getSurface());
+        mMediaPlayer.init(/*mVideoPath*/"rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4", VIDEO_RENDER_ANWINDOW, surfaceHolder.getSurface());
     }
 
     @Override
